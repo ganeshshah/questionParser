14d85e961a58ffb8344cc298223a8dfc65e63076
Surya Sonar <iamsuryasonar@gmail.com>
2023-09-28 13:29:09 +0530

    replaced navbar to sidebar for better ux

--- src/main/userinterface/src/views/Home.js
+++ src/main/userinterface/src/views/Home.js
@@ -3,7 +3,7 @@ import './Home.css';
 import { Link } from 'react-router-dom';
 import QuestionLoader from './loadQuestions/QuestionLoader';
 import { useNavigate } from 'react-router-dom';
-
+import QuestionCard from '../components/QuestionCard';
 
 function Home() {
 
@@ -12,69 +12,21 @@ function Home() {
     navigate('/loadQuestions');
   };
 
+  const nums = [1, 2, 3];
+
   return (
     <div className="Home">
       <h1>MCQ Practice</h1>
       <h3>Created this portal to do spaced repetition</h3>
-      <div>
-        <ul className="menu-list">
-          <li>
-            {/* 
-            <Link to="/Finance">Finance</Link>
-          </li>
-          <li>
-            <Link to="/management">Management</Link>
-          </li>
-          <li>
-            <Link to="/esi">ESI</Link>
-          </li>
-          <li>
-            <Link to="/PIB24x7">PIB24x7</Link>
-          </li>
-          <li>
-            <Link to="/RBI24x7">RBI24x7</Link>
-          </li>
-          <li>
-            <Link to="/SpotlightGA">General Awareness</Link>
-          </li>
-          <li> */}
-            <Link to="/CloudAffairsGA">Cloudaffairs GA</Link>
-          </li>
-          <li>
-            <Link to="/esi">Create Mixed Test</Link>
-          </li>
-          <li>
-            <Link to="/esi">Analytics dashboard</Link>
-          </li>
-        </ul>
-        <div>
-          <button className='buttons'>Add Question Manually</button>
-          <button className='buttons'> Load Question from excel</button>
-          <button className='buttons' onClick={navigateLoadQuestions}>Load Question from text file</button>
-        </div>
-      </div>
+      <div className='mx-auto my-5 max-w-5xl flex flex-col gap-y-8'>
+        {
+          nums.map(() => (<QuestionCard />))
+        }
+      </div >
     </div >
   );
 }
 
 export default Home;
 
-
-
-// import QuestionCard from '../components/QuestionCard';
-
-// function Home() {
-
-//   const nums = [1, 2, 3, 4, 5];
-
-//   return (
-//     <div className='mx-auto my-5 max-w-5xl flex flex-col gap-y-8'>
-//       {
-//         nums.map(() => (<QuestionCard />))
-//       }
-//     </div >
-//   );
-// }
-
-// export default Home;
 
